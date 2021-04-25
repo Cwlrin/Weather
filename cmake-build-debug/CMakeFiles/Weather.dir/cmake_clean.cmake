a8b7f3083cb753f6420f7ca4e856fd86a9ac505d
Cwlrin <20304773@qq.com>
2021-04-25 21:12:37 +0800

    修正CMake

---
+++ cmake-build-debug/CMakeFiles/Weather.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Weather.dir/cJSON.c.obj"
+  "CMakeFiles/Weather.dir/main.cpp.obj"
+  "CMakeFiles/Weather.dir/utf8togbk.c.obj"
+  "Weather.exe"
+  "Weather.exe.manifest"
+  "Weather.pdb"
+  "libWeather.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Weather.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
